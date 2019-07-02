77840e67c3878ca311b582aa98fe3b865cd15d31
sunilpimenta <snpimenta@gmail.com>
2019-07-01 17:03:22 -0700

    Add files via upload

---
+++ ex2/ex2.m
@@ -0,0 +1,107 @@
+% Load Data
+% The first two columns contain the exam scores and the third column contains the label.
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+% Plot the data with + indicating (y = 1) examples and o indicating (y = 0) examples.
+plotData(X, y);
+ 
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+
+%  Setup the data matrix appropriately
+[m, n] = size(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Initialize the fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display the initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+disp('Gradient at initial theta (zeros):'); disp(grad);
+
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+% Run fminunc to obtain the optimal theta
+% This function will return theta and the cost
+[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Print theta
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+
+disp('theta:');disp(theta);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+
+% Add some labels 
+hold on;
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+
+
+%  Predict probability for a student with score 45 on exam 1  and score 85 on exam 2 
+prob = sigmoid([1 45 85] * theta);
+fprintf('For a student with scores 45 and 85, we predict an admission probability of %f\n\n', prob);
+
+% REGULARIZATION
+
+%  The first two columns contains the X values and the third column
+%  contains the label (y).
+data = load('ex2data2.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+plotData(X, y);
+% Put some labels 
+hold on;
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+% Specified in plot order
+legend('y = 1', 'y = 0')
+
+hold off;
+
+X = mapFeature(X(:,1), X(:,2));
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1
+lambda = 100;
+
+% Compute and display initial cost and gradient for regularized logistic regression
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+options = optimset('GradObj', 'on', 'MaxIter', 1000);
+
+[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+
+hold on;
+title(sprintf('lambda = %g', lambda))
+
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+
+legend('y = 1', 'y = 0', 'Decision boundary')
+
+hold off;
+
+
